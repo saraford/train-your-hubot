1163601a3beface70e15c27d9db51e747db7cc97
Sara Ford <saraford@users.noreply.github.com>
2016-08-04 09:54:01 -0700

    renaming variables

--- renderer.js
+++ renderer.js
@@ -43,16 +43,16 @@ function spawnHubot() {
   const spawn = require('child_process').spawn;
   hubot_spawn = spawn(hubot_path, {cwd: hubot_cwd_path, env: process.env});
 
-  let hubotOutput = $('#hubot-output');
+  let hubotOutputWindow = $('#hubot-output');
 
   var hubotLoaded = false;
-  var current_raw_response = "";
+  var current_response = "";
   hubot_spawn.stdout.on('data', (data) => {
 
     if (data.indexOf("Data for hubot brain retrieved from Redis") !== -1) {
       hubotLoaded = true;
 
-      hubotOutput.append("<div class='hubot-msg'>myhubot ready</div>");
+      hubotOutputWindow.append("<div class='hubot-msg'>myhubot ready</div>");
       return;
     }
 
@@ -66,18 +66,18 @@ function spawnHubot() {
 
     // because hubot is echo'ing commands back, we have to wait until we get "myhubot2>"
     // to avoid displaying that text
-    current_raw_response += hubot_response;
+    current_response += hubot_response;
 
     // the response we want comes after the current "myhubot>" response
-    if (current_raw_response.includes("myhubot>")) {
+    if (current_response.includes("myhubot>")) {
       is_response_next = true;
 
-      // the response might have come with this current_raw_response
-      // e.g. "myhubot> PONG" instead of PONG on a newline
+      // the real hubot response might have come with this current_response
+      // e.g. "myhubot> PONG" instead of "PONG" on a newline
       // so display everything that comes after "myhubot>"
-      var index = current_raw_response.indexOf("myhubot>");
-      var length = current_raw_response.length;
-      var start_of_response = current_raw_response.substring(index + 8, length - 1);
+      var index = current_response.indexOf("myhubot>");
+      var length = current_response.length;
+      var start_of_response = current_response.substring(index + 8, length - 1);
 
       console.log("index: " + index + " lenght: " + length + " start_of_response: " + start_of_response);
 
@@ -85,39 +85,39 @@ function spawnHubot() {
         console.log("Yep response was on same line: " + start_of_response);
 
         // trim whitespace and add
-        hubotOutput.append("<div class='hubot-msg'>" + start_of_response.trim() + "</div>");
+        hubotOutputWindow.append("<div class='hubot-msg'>" + start_of_response.trim() + "</div>");
 
         // to keep the latest output visible
-        hubotOutput.stop().animate({
-          scrollTop: hubotOutput[0].scrollHeight
+        hubotOutputWindow.stop().animate({
+          scrollTop: hubotOutputWindow[0].scrollHeight
         }, 200);
 
         // clear current response for next time
-        current_raw_response = "";
+        current_response = "";
         return;
 
       } else {
 
         // hubot just echo'ing back our previous text
         // just ignore this stdout.on event and continue to next event
-        current_raw_response = "";
+        current_response = "";
         return;
       }
 
     }
 
     if (is_response_next) {
-      console.log("This is next response " + current_raw_response);
+      console.log("This is next response " + current_response);
 
       // prep found text and add
-      hubotOutput.append("<div class='hubot-msg'>" + hubot_response.trim() + "</div>");
+      hubotOutputWindow.append("<div class='hubot-msg'>" + hubot_response.trim() + "</div>");
 
       // to keep the latest output visible
-      hubotOutput.stop().animate({
-        scrollTop: hubotOutput[0].scrollHeight
+      hubotOutputWindow.stop().animate({
+        scrollTop: hubotOutputWindow[0].scrollHeight
       }, 200);
 
-      current_raw_response = "";
+      current_response = "";
       console.log("stdout: " + hubot_response.trim());
     }
 
